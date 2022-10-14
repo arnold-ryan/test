29a59f2bf7488978d35a23d0850bea2b2de581e3
Arnold Odhiambo <arnold.ryan801@gmail.com>
2022-10-14 16:29:21 +0300

    created a function

---
+++ test.js
@@ -0,0 +1,11 @@
+function callMyName(name) {
+  return "Hallo, " + name;
+}
+
+console.log(callMyName("Arnold"));
+
+const shoutName = (name) => {
+  console.log(name.toUpperCase());
+};
+
+shoutName("arnold");
